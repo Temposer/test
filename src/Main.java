f02c9ef8526610962fd0307675324171d804e198
zhaols <icansyou@163.com>
2024-01-05 20:26:49 +0800

    第三次

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-
+        title tt = new title();
         System.out.println("Hello world!");
         System.out.println("Hello 111111111111111!");
+        tt.title1();
     }
 }
---
+++ src/title.java
@@ -0,0 +1,5 @@
+public class title {
+    public void title1(){
+        System.out.println("sssssssssssssss");
+    }
+}
